83b2584971feb4389a2051d8dba363c9bb497cf4
Lukyunenko Yaroslav <l.yaruk1993@gmail.com>
2015-12-19 01:34:18 +0200

    Change name of DAO package

--- src/main/java/com/Controller/GetterController.java
+++ src/main/java/com/Controller/GetterController.java
@@ -1,7 +1,7 @@
 package com.Controller;
 
 import com.Model.User;
-import com.DAO.UserService;
+import com.dao.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
